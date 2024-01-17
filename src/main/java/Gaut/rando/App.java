431d77553c20e590001d737f566f577ba77aedfc
gautamikomal <strictlyforwork18@gmail.com>
2024-01-17 13:14:50 +0530

    sE push

---
+++ src/main/java/Gaut/rando/App.java
@@ -0,0 +1,13 @@
+package Gaut.rando;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
